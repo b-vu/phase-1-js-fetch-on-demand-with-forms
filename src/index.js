cda0020e53c5aea3ee7fa1d9b2878eb727f14775
Brian Vu <brianvu7@gmail.com>
2022-02-07 21:26:47 -0600

    commit tree 6c62d563f4d4bfd09608cceb60dec4fe45778cfa

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+  const search = document.querySelector("form");
+  search.addEventListener("submit", e => {
+      e.preventDefault();
+      console.log(e.target["searchByID"].value);
+
+      fetch(`http://localhost:3000/movies/${e.target["searchByID"].value}`)
+      .then(res => res.json())
+      .then(data => displayMovie(data));
+  });
+}
+
+const displayMovie = data => {
+    console.log(data);
+    const movieDetails = document.querySelector("#movieDetails");
+    console.log(movieDetails.childNodes);
+    movieDetails.childNodes[1].textContent = data.title;
+    movieDetails.childNodes[3].textContent = data.summary;
 }
 
 document.addEventListener('DOMContentLoaded', init);
